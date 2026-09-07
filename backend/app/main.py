b6780a648fd3019218baef352ecabc8feb8fa65f
Ubuntu <ubuntu@ip-172-31-12-145.ap-southeast-2.compute.internal>
2026-09-07 17:08:52 +0000

    add api domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,17 +33,19 @@ app = FastAPI(
 app.include_router(auth_router)
 app.include_router(profile_router)
 app.include_router(discovery_router)
+
+origins = [
+    "http://localhost:3000",
+    "https://devscoutai.c4you.in",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 class SearchRequest(BaseModel):
     query: str = Field(
